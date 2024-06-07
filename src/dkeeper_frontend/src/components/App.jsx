0db4c800e8cada7aa01d3219e12162ab23f1d9e0
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-06-07 13:49:29 +0300

    initial commit. Frontend is connected to motoko backend

---
+++ src/dkeeper_frontend/src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import {dkeeper_backend} from "../../../declarations/dkeeper_backend";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      dkeeper_backend.createNote(newNote.title, newNote.content);
+      console.log(123);
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
